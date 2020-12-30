40cd5651cdd2a3ea49dbf632fdfc6d416d3bb4e5
plotnikov.s <onlygrach@gmail.com>
2020-12-30 10:42:51 +0500

    new

--- create.tf
+++ create.tf
@@ -65,6 +65,32 @@ resource "yandex_vpc_subnet" "subnet-1" {
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
 
+
+resource "yandex_vpc_security_group" "group1" {
+  name        = "group1"
+  description = "allow web"
+  network_id  = "${yandex_vpc_network.network-1.id}"
+
+  labels = {
+    my-label = "group1"
+}
+
+  ingress {
+    protocol       = "ANY"
+    description    = "web"
+    v4_cidr_blocks = ["192.168.10.0/24"]
+    port           = 8080
+}
+
+  egress {
+    protocol       = "ANY"
+    description    = "web"
+    v4_cidr_blocks = ["192.168.10.0/24"]
+    port           = 8080
+  }
+}
+
+
 resource "local_file" "AnsibleInventory" {
   content = templatefile("inventory.tmpl",
     {
